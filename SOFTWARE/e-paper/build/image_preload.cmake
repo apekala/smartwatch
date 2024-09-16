35c1e56889a0abf3ed39c9b06161ad7187bacc15
Antoni Pekala <antonipekala@outlook.com>
2024-09-17 02:44:02 +0300

    delete temp files

--- SOFTWARE/e-paper/build/image_preload.cmake
+++
@@ -1,7 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "nrf52833dk_nrf52833" CACHE INTERNAL "NCS child image controlled")
-set(CACHED_CONF_FILE "d:/franz/Documents/studia/eity/PBL4/smartwatch2/smartwatch/SOFTWARE/e-paper3/prj.conf" CACHE INTERNAL "NCS child image controlled")
-set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "d:/franz/Documents/studia/eity/PBL4/smartwatch2/smartwatch/SOFTWARE/e-paper3/epd.overlay" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "D:/aplikacje/nRF/toolchains/cf2149caf2/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
